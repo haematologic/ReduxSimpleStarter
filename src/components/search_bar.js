2fd2e6f0be0474e1f80a286ee3e31709d7894965
Duncan Brian <haematologic+github@gmail.com>
2017-04-15 10:24:37 +0100

    app completed with style and debounce

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,18 +4,24 @@ class SearchBar extends Component {  // ES6 classes based React component
     constructor(props) {  // always called when class initated
         super(props);  // super calls parent method on parent class (Component)
 
-        this.state = { term: '' };  // state - plain JS obj, initialised by defining a property. 
+        this.state = { term: '' };  // state - plain JS obj, initialised by defining a property.
+        // component level state (SearchBar) 
     }
     
     render() {  // render method = function
         return (
-            <div>
+            <div className="search-bar">
                 <input
                     value={this.state.term} // 'controlled component' - new value re-rendered when state updated
-                    onChange={event => this.setState({ term: event.target.value })}  //ES6 arrow func tidys up 
+                    onChange={event => this.onInputChange(event.target.value)}  //ES6 arrow func tidys up 
                 />
             </div>
         )  // javascript variables in JSX enclosed in {}
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -6,6 +7,7 @@ import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCSJiSWJH5q8z3qrJO_Oi_4Ys3ohpFmjJ4';
 
+// class allows concept of state to be used, rather than function, returns static jsx
 class App extends Component {
     constructor(props) { // constructor always called with props
         super(props);
@@ -14,9 +16,13 @@ class App extends Component {
             videos: [],
             selectedVideo: null
         };  // array of objects
-
+        
+        this.videoSearch('surfboards')
         // search func into constructor to avoid totally empty list on init
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => { // callback function (with data)
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => { // callback function (with data)
             this.setState({  // causes rerendering
                 videos: videos,
                 selectedVideo: videos[0]
@@ -24,14 +30,19 @@ class App extends Component {
             console.log(videos);
         });
     }
+
     render () {
+        // debounced version
+       const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
        return ( // App is parent of VideoList - need to pass data parent to child 'passing props - prop videos'
        // props argument will arrive as argument to a function 
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange={videoSearch} /> 
                 <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
-                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) } 
+                    // passed callback between App and grandchild - limit of confusing
                     videos={this.state.videos} />
             </div>
         );
