19d4cabee7592415503c632170761979a5644e8d
Duncan Brian <haematologic+github@gmail.com>
2017-04-15 20:04:29 +0100

    BookList working as container, connected with mapStateToProps

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,14 +1,10 @@
 //only purphose - to render a list of books
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
     renderList() {
-        return this.props.books.map((book) => {  // need to plug-in application state to here
-            // to combine React views and Redux state - connecting the two libraries is done with a separate library
-            // called React-Redux. To do this make one Component to being a Container (a React component which has a
-            // direct connection to the state managed by Redux). React-Redux is the bridge.
-            // Containers are 'smart' Components and can inject state into 'dumb' Components.
-            // Only tghe most parent component using a particular piece of state needs to be connected to Redux.
+        return this.props.books.map((book) => {
             return (
                 <li key={book.title} className="list-group-item">{book.title}</li>
             );
@@ -16,6 +12,7 @@ export default class BookList extends Component {
     }
 
     render() {
+        // console.log(this.props.asdf)  // -> 123
         return (
             <ul className="list-group col-sm-4">
                 {this.renderList()}
@@ -23,3 +20,13 @@ export default class BookList extends Component {
         )
     }
 }
+
+function mapStateToProps(state) {  // takes App state as an argument, on any change instantly re-renders
+    // Whatever is return will show up as props inside BookList
+    return {  // an object
+        // asdf: '123',
+        books: state.books  // connection between Redux and container populates this.props.books with state.books
+    }
+}
+
+export default connect(mapStateToProps)(BookList);  // connect(function)(component) -> container
